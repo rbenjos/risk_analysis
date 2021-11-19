d5b81c470940e67e6fab63c20a08db3d691d639d
rbenjos <rbenjos@gmail.com>
2021-11-19 13:23:26 +0200

    Removed folder from repository

--- rater.bat
+++
@@ -1,9 +0,0 @@
-python C:\Users\Administrator\Desktop\OPC\risk_analysis\risk_rater_2.py C:\Users\Administrator\Desktop\OPC\April-19 515
-
-python C:\Users\Administrator\Desktop\OPC\risk_analysis\risk_rater_2.py C:\Users\Administrator\Desktop\OPC\April-20 515
-
-python C:\Users\Administrator\Desktop\OPC\risk_analysis\risk_rater_2.py C:\Users\Administrator\Desktop\OPC\May-20 515
-
-python C:\Users\Administrator\Desktop\OPC\risk_analysis\risk_rater_2.py C:\Users\Administrator\Desktop\OPC\July-20 515
-
-pause
